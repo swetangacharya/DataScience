8bfef359ffc4c399d90842524e0cb683f6f035ac
swetangacharya <63034325+swetangacharya@users.noreply.github.com>
2025-09-15 14:39:37 +0530

    Update Dendrogram1.py

--- Clustering/Dendrogram1.py
+++ Clustering/Dendrogram1.py
@@ -1,4 +1,5 @@
 from scipy.cluster import hierarchy
+import numpy as np
 import matplotlib.pyplot as plt
 X=np.array([[0.4005,0.5306],[0.2148,0.3854],[0.3457,0.3156],[0.2652,0.1875],[0.0789,0.4139],[0.4548,0.3022]])
 temp = hierarchy.linkage(X, 'single')
